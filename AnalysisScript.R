fae5286f42b5bf8bd0a939ff6b660522c3a710f3
AndrlmMass <a.l.massey@medisin.uio.no>
2022-06-19 15:54:13 +0200

    IQR testing and violin plots in for loop.

--- AnalysisScript.R
+++ AnalysisScript.R
@@ -6,39 +6,41 @@ attach(df)
 
 # First, locate the outliers
 
-ViolinIQR <- function(Item){
+ViolinIQR <- function(Item,Colnam){
+  print(Colnam)
   print(ggplot(df, aes(x=Condition,y=Item,fill=Condition))+
           geom_violin(trim =FALSE)+
           stat_summary(fun = "mean", geom = "crossbar", width = 0.4, colour = "black")+
-          geom_point(position = position_jitter(w=0.1, h=0)))
+          geom_point(position = position_jitter(width = .03, height = .01))+
+          ggtitle(Colnam))
   IQROutliers <- (boxplot.stats(Item)$out)
   return (IQROutliers)
-}
-
-ViolinIQR(TypeOfTouch)
+  }
 
 #Loop through all the relevant columns to check for outliers
 
 #Specify which columns are relevant
 ColList <- c(2,4,6,9:16,18,20,22:70,74:86)
+colnamnam <- c() 
 
+for (i in 1:length(ColList)){
+  u <- ColList[i]
+  t <- names(df[u])
+  colnamnam <- append(colnamnam,t)
+}
+
+#Loop through the columns by pressing "1" to move forward
 Approv <- 1
 for (i in 1:length(ColList)){
-  if (Approv != 1){
+  ColumnVec <- df[,ColList[i]]
+  ColumnName <- colnamnam[i]
+  ViolinIQR(ColumnVec,ColumnName)
+  Approv <- readline(prompt = "Press enter to see the next plot.")
+  if (Approv != ""){
     break
-  } else {
-    Column <- df[,ColList[i]]
-    ViolinIQR(Column)
-    Approv <- readline(prompt = "Do you want to check the next column? Press 1, exit by pressing anything else.")
-  }
+  } 
 }
 
 
-
-for (i in 1:5){
-  if (i > 3){
-    break
-  } else if
-    print(i)
-}
+  
 
