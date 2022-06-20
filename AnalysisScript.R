a5d8eba7edac3f07431250a7bd8e4e3dda7a3785
AndrlmMass <a.l.massey@medisin.uio.no>
2022-06-20 23:34:44 +0200

    Added lm and check assumptions

--- AnalysisScript.R
+++ AnalysisScript.R
@@ -7,20 +7,19 @@ attach(df)
 # First, locate the outliers
 
 ViolinIQR <- function(Item,Colnam){
-  print(Colnam)
   print(ggplot(df, aes(x=Condition,y=Item,fill=Condition))+
           geom_violin(trim =FALSE)+
           stat_summary(fun = "mean", geom = "crossbar", width = 0.4, colour = "black")+
-          geom_point(position = position_jitter(width = .03, height = .01))+
+          geom_point(position = position_jitter(width = .07, height = .5))+
           ggtitle(Colnam))
   IQROutliers <- (boxplot.stats(Item)$out)
   return (IQROutliers)
-  }
+}
 
 #Loop through all the relevant columns to check for outliers
 
 #Specify which columns are relevant
-ColList <- c(2,4,6,9:16,18,20,22:70,74:86)
+ColList <- c(2,4,6,9:16,18,20,69:70,72,74:86)
 colnamnam <- c() 
 
 for (i in 1:length(ColList)){
@@ -40,6 +39,18 @@ for (i in 1:length(ColList)){
     break
   } 
 }
+
+## Does Needs predict affect?
+
+plot(NegativeAff, GrandNeedMean)
+
+lm1 <- lm(NegativeAff ~ GrandNeedMean)
+
+summary(lm1)
+
+par(mfrow=c(2,2))
+plot(lm1)
+
 
 
   
